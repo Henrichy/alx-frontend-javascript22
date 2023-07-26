4a32f86b78f3ab70c6518fd562591f60acd28464
Henrichy <107367296+Henrichy@users.noreply.github.com>
2023-07-26 17:22:17 +0100

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,31 +1,40 @@
-// Assuming you have the required libraries and modules imported
-import { uploadPhoto, createUser } from './utils.js';
 
-function getResponseFromAPI() {
-  // Replace the API call below with the actual API call you want to make.
-  // In this case, we are calling the `uploadPhoto` and `createUser` functions from utils.js
-  return new Promise((resolve, reject) => {
-    // First, call uploadPhoto
-    utils.uploadPhoto(photoData)
-      .then(uploadPhotoResponse => {
-        // If uploadPhoto is successful, call createUser
-        utils.createUser(userData)
-          .then(createUserResponse => {
-            // If both uploadPhoto and createUser are successful, resolve with the combined responses
-            resolve({
-              uploadPhoto: uploadPhotoResponse,
-              createUser: createUserResponse,
-            });
-          })
-          .catch(error => {
-            // If there's an error in createUser, reject with the error
-            reject(error);
-          });
-      })
-      .catch(error => {
-        // If there's an error in uploadPhoto, reject with the error
-        reject(error);
-      });
-  });
-}
+ import { uploadPhoto, createUser } from './utils';
+ 
+ /*
+ const uploadPhotoVar = uploadPhoto();
+ const createUserVar = createUser();
+ */
+ 
+ export default function handleProfileSignup() {
+   const uploadPhotoVar = uploadPhoto();
+   const createUserVar = createUser();
+   /*
+   let body;
+   let firstName;
+   let lastName;
+   */
+ 
+   return Promise.all([uploadPhotoVar, createUserVar])
+     .then((data) => {
+       console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
+     }).catch(() => {
+       console.log('Signup system offline');
+     });
+ 
+   /*
+   return uploadPhotoVar.then((data) => {
+     body = data.body;
+     return createUserVar;
+   }).then((data) => {
+     firstName = data.firstName;
+     lastName = data.lastName;
+     console.log(body, firstName, lastName);
+   }).catch(() => {
+     console.log('Signup system offline');
+   });
+   */
+ }
+ 
+ // handleProfileSignup();
 
